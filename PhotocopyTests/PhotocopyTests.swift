ab818742aae2ad08992d8dd5b9a9165ee28840d4
Rafael Costa <rafael@rafaelcosta.me>
2025-05-25 17:34:50 +0200

    Initial Commit

---
+++ PhotocopyTests/PhotocopyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotocopyTests.swift
+//  PhotocopyTests
+//
+//  Created by Rafael Costa on 2025-05-25.
+//
+
+import Testing
+@testable import Photocopy
+
+struct PhotocopyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
